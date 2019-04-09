4f873ccd8ff5641008a7baccccc088c5fe82a28b
Isabelle David <isabelledpauladavid@gmail.com>
2019-04-09 16:38:40 -0300

    * Modificação da navbar; * Criação da estrutura dos cards.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/home/index';
 import MainService from './services/MainService';
 import registerServiceWorker from './registerServiceWorker';
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import './style.css'
+import logoImage from '../../assets/images/adhere_logo.svg'
 
 class Home extends React.Component {
 
     render() {
         return (
-            <body class="body">
+            <body className="body">
                 {/* Div da NavBar */}
-                <div class="divNav">
-                    <nav class="navbar">
-                        <a id="a1" href="#menu">AdHere</a>
-                        <a id="a2" href="#">Belém</a>
-                    </nav>
-                </div>
-
-                {/* Div da NavBar */}
-
-                <div class="card">
-                    <div class="container">
-                        <h4>Melhor lugar pra por o bagulho</h4>
+                <div className="container1">
+                    <div className="divNav">
+                        <nav className="navbar">
+                            <img src={logoImage} height="30" />
+                            <a id="a2" href="#">Belém</a>
+                        </nav>
                     </div>
+
+                    {/* Menu column 1 */}
+
+                    <section id="column1">
+                        <div className="card">
+                            <h4>Melhor lugar pra por o bagulho</h4>
+                        </div>
+
+
+                    </section>
+
+                    <section id="column2">
+                        <div className="card">
+                            <h4>Melhor lugar pra por o bagulho</h4>
+                        </div>
+                    </section>
+
+                    {/* <div class="column2">
+
+                    </div> */}
                 </div>
-            </body>
+
+            </body >
 
         );
     }
