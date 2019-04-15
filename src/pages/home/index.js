16735707ae7a7f73b56979b63d77fc1b806d1248
tellesvh <tellesvictorhugo@gmail.com>
2019-04-15 11:28:22 -0300

    Ajustes para futuras implementações

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { HereMap, Marker, Polygon, Polyline, Circle, Rectangle, PathFinder } from 'rc-here-maps';
+// import HEREMap, { Marker, RouteLine } from 'here-maps-react'
+import { HereMap, Marker, PathFinder } from 'rc-here-maps';
 import './style.css'
+import axios from 'axios'
 import logoImage from '../../assets/images/adhere_logo.svg'
 
 class Home extends Component {
@@ -9,41 +11,43 @@ class Home extends Component {
         super(props);
 
         this.state = {
-            lat: -1.4578913,
-            lng: -48.4734453,
             center: {
                 lat: -1.4578913,
                 lng: -48.4734453,
             },
-            showPopover: false,
-            polylineDataPoints: [10, 3, 100, 25, 13, 100, 10, 30, 100, 25, 25, 100],
-        };
+
+            requestHERE: null,
+
+            streetBlocks: null,
+        }
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            const coords = {
-                lat: -1.4578913,
-                lng: -48.4734453,
-            };
-            this.setState({
-                lat: coords.lat,
-                lng: coords.lng,
-                center: coords,
-                polylineDataPoints: [10, 3, 100, 25, 13, 100, 10, 30, 100, 25, 25, 100]
-            });
-        }, 1000);
+        axios.get("http://moobi-heremap.herokuapp.com/?key=PIMoobi2019", {
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(res => {
+                const requestHERE = res.data;
+                this.setState({ requestHERE });
+            })
     }
 
-    onTogglePopover = showPopover => {
-        this.setState({
-            showPopover,
-        });
-    };
+    handleCoordinates = (coordinates) => {
+        var finalCoordinates = [];
 
-    onClick = () => {
-        console.log('I am clicked');
-    };
+        coordinates.map(coordinate => {
+            var subCoordinates = coordinate.split(" ");
+
+            subCoordinates.map(subCoordinate => {
+                if (subCoordinate !== "") {
+                    finalCoordinates.push(subCoordinate);
+                }
+            });
+        })
+
+        return finalCoordinates;
+    }
+
     render() {
         return (
             <body className="body">
@@ -60,7 +64,7 @@ class Home extends Component {
 
                     <section id="column1">
                         <div className="card">
-                            <p>Melhores pontos para anunciar nas <b>sextas</b>, ás <b>08h:</b></p>
+                            <p>Melhores pontos para anunciar nas <b>sextas</b>, às <b>08h:</b></p>
                             <ul id="lista">
                                 <li>
                                     <div class="linha-vertical">
@@ -80,42 +84,74 @@ class Home extends Component {
                             </ul>
                         </div>
 
-
-                        <div className="App">
-                            <HereMap
-                                appId="my2dhMRU9k56Wv8QdloT"
-                                appCode="dGOv83JgdKNfBmMakPG5RA"
-                                useHTTPS={false}
-                                center={this.state.center}
-                                zoom={14}>
-
-                                <Circle
+                        <div className="cardMap" style={{ padding: 4 }}>
+                            <div className="App">
+                                {/* <HEREMap
+                                    appId="my2dhMRU9k56Wv8QdloT"
+                                    appCode="dGOv83JgdKNfBmMakPG5RA"
                                     center={this.state.center}
-                                    radius={100}
-                                    fillColor="rgba(256, 0, 0, 0.9)"
-                                    strokeColor="rgba(100, 0, 256, 0.9)"
-                                    onCircleDrawn={this.onCircleDrawn}
-                                />
-                                <Marker lat={-1.470165} lng={-48.468739}>
-                                    <i class="material-icons">
-                                        dock
-                                    </i>
-                                </Marker>
+                                    zoom={13}
+                                    hidpi
+                                    interactive
+                                    // setLayer={{ layer: 'traffic', mapType: 'terrain' }}>
+                                    setLayer={{ layer: 'map', mapType: 'terrain' }}>
 
-                                <Polyline dataPoints={this.state.polylineDataPoints} />
+                                    {this.state.requestHERE != null ?
+                                        this.state.requestHERE.dados.ruas.cruzamentos.map(cruzamento => {
 
-                                <PathFinder
-                                    waypoints={[{ lat: -1.454180, lng: -48.496663 }, { lat: -1.470165, lng: -48.468739 }]}
-                                    style={{
-                                        lineWidth: 5,
-                                        strokeColor: 'rgba(256, 0, 0, 0.9)',
-                                    }}
-                                />
-                            </HereMap>
-                        </div >
+                                            return cruzamento.ruasTransversais.map(rua => {
+                                                // { console.log(rua.nomeRuaTransversal) }
+                                                // { console.log("---") }
+                                                // { console.log(this.handleCoordinates(rua.pontosDeEncontro)) }
 
+                                                var trafficFlow = rua.fluxoAtual[0].nivelDeTrafego;
+                                                var color = "";
 
+                                                if (trafficFlow >= 0 && trafficFlow < 4) {
+                                                    color = "#00c853";
+                                                } else if (trafficFlow >= 4 && trafficFlow < 8) {
+                                                    color = "#ff6f00"
+                                                } else if (trafficFlow >= 8 && trafficFlow < 10) {
+                                                    color = "#b71c1c"
+                                                } else if (trafficFlow === 10) {
+                                                    color = "#000000"
+                                                }
 
+                                                return (
+                                                    <RouteLine
+                                                        shape={this.handleCoordinates(rua.pontosDeEncontro)}
+                                                        strokeColor={color}
+                                                        lineWidth={4}
+                                                    />)
+                                            })
+                                        })
+                                        : null}
+
+                                </HEREMap> */}
+
+                                <HereMap
+                                    appId="my2dhMRU9k56Wv8QdloT"
+                                    appCode="dGOv83JgdKNfBmMakPG5RA"
+                                    useHTTPS={false}
+                                    center={this.state.center}
+                                    zoom={13.5}>
+
+                                    <Marker lat={-1.470165} lng={-48.468739}>
+                                        <i class="material-icons">
+                                            dock
+                                        </i>
+                                    </Marker>
+
+                                    <PathFinder
+                                        waypoints={[{ lat: -1.454180, lng: -48.496663 }, { lat: -1.470165, lng: -48.468739 }]}
+                                        style={{
+                                            lineWidth: 5,
+                                            strokeColor: 'rgba(256, 0, 0, 0.9)',
+                                        }}
+                                    />
+                                </HereMap>
+                            </div>
+                        </div>
                     </section>
 
                     <section id="column2">
